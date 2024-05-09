0ad029374de7e32e27faf142200282f144f9d4fd
griseldasanchez <gs.griseldasanchez@gmail.com>
2024-05-09 01:28:39 -0700

    Setup react router

--- app/src/App.js
+++ app/src/App.js
@@ -1,99 +1,65 @@
-import { useState } from "react";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-} from "firebase/auth";
-import "./App.css";
-import { auth } from "./firebase.js";
+import React from 'react';
+import { createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  Link, 
+  Outlet, 
+  RouterProvider 
+} from 'react-router-dom';
+
+// Components
+import Home from './Home';
+import About from './About';
+import NavigationBar from './NavigationBar.js';
+import SignUpForm from './SignUpForm';
+
+// Styles
+import './styles/App.scss';
 
 function App() {
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
 
-  const [user, setUser] = useState({});
-
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
-  });
-
-  const register = async () => {
-    try {
-      const user = await createUserWithEmailAndPassword(
-        auth,
-        registerEmail,
-        registerPassword
-      );
-      console.log(user);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const login = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginEmail,
-        loginPassword
-      );
-      console.log(user);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const logout = async () => {
-    await signOut(auth);
-  };
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root /> }>
+        <Route index element={<SignUpForm />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Route>
+    )
+  );
 
   return (
-    <div className="App">
-      <div>
-        <h3> Register User </h3>
-        <input
-          placeholder="Email..."
-          onChange={(event) => {
-            setRegisterEmail(event.target.value);
-          }}
-        />
-        <input
-          placeholder="Password..."
-          onChange={(event) => {
-            setRegisterPassword(event.target.value);
-          }}
-        />
+    
+      <div id="App">
+        
+        <div id="app-body">
+          <div>
+            <RouterProvider router={router} />        
+          </div>
+        </div>
 
-        <button onClick={register}> Create User</button>
+        <footer id="app-footer">
+        </footer>
       </div>
 
-      <div>
-        <h3> Login </h3>
-        <input
-          placeholder="Email..."
-          onChange={(event) => {
-            setLoginEmail(event.target.value);
-          }}
-        />
-        <input
-          placeholder="Password..."
-          onChange={(event) => {
-            setLoginPassword(event.target.value);
-          }}
-        />
-
-        <button onClick={login}> Login</button>
-      </div>
-
-      <h4> User Logged In: </h4>
-      {user?.email}
-
-      <button onClick={logout}> Sign Out </button>
-    </div>
   );
+}
+
+export const Root = () => {
+  return (
+    <>
+    {/* Navigation Bar Routes */}
+      <div id="navigation-bar"> 
+        <Link to="/">Login</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+    {/* Other Routes */}
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
 }
 
 export default App;
