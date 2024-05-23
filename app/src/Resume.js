6dafe86c378009ad9e38e06a658b1a7bdf06ff78
griseldasanchez <gs.griseldasanchez@gmail.com>
2024-05-22 17:38:50 -0700

    Display resume

--- app/src/Resume.js
+++ app/src/Resume.js
@@ -1,10 +1,17 @@
 import React from "react";
+import './styles/Resume.scss';
 
 function Resume() {
 
+  const docUrl = "https://docs.google.com/document/d/1sYl6Jevm_kP1qp0FC0CQWiZfnELCW3eTvOYEOiUaUqY/preview"; // Replace with your embed link
+
   return (
-    <div>
-      Here is my resume
+    <div id="resume">
+        <iframe
+          src={docUrl}
+          allow="autoplay"
+          title="Google Docs Viewer"
+        ></iframe>
     </div>
   )
 
